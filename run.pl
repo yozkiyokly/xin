1d7d93d4c20001bc996e4136e6f067e65bf25f6c
yozki-yokly <yozki@yandex.ru>
2023-10-15 05:09:38 +0700

    Причесали промежуточный результат.

--- run.pl
+++ run.pl
@@ -1,15 +1,21 @@
 #!/usr/bin/perl
 
+# Это файл-запускалка. 
+# Все отладочные выкрутасы и комментарии разбора кода  
+# лежат в файле SlotsShort.pm, a asset.pl локально - лишь симлинк. 
+# Они идентичны, а на гите лежат оба на случай, если потребуется рассмотреть 
+# процесс разбора по отдельным коммитам. Мало ли вдруг это кому-то захочется.
+
+
 use Data::Dumper qw(Dumper); # Чисто ради выпечатывания массивов
  
 
 
 use lib './';
 use SlotsShort qw (slots_unshort);
+print "Go on, delayedDiskWrite!\n";
 
-print "go on, delayedDiskWrite!\n";
+# Тестовый набор данных передаём прямо в вызове:
 @unshorterArray= slots_unshort(' 23 , 33, 5-4   , 8 , 4-11, 4, 54 , 23 , 3-8 , 1-1 , 2-2 , ');
-print "\ndirect array try:\n";
-print @unshorterArray;
-print "\nthen dumper arr print:\n";
+print "\ndumper arr print:\n";
 print Dumper \@unshorterArray;
